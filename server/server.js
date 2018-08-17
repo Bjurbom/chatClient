39796a6f578748a03b901b1acedc5083c8d500e1
tor bjurbom <bjurbom1@gmail.com>
2018-08-17 21:29:32 +0200

    added io socket

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 
 const port = process.env.PORT || 3000
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 
 const publicPath = path.join(__dirname,'../public');
@@ -15,11 +20,18 @@ console.log(publicPath);
 
 app.use(express.static(publicPath));
 
-//index filen
+io.on('connection' , (socket) =>{
+ console.log('New user connected');
+
+    socket.on('disconnect' , () =>{
+        console.log("User have disconnected");
+    });
+ });
 
 
 
 
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`)
+
+server.listen(port, ()=>{
+    console.log(`server is running on ${port}`);
 });
