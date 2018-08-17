b54664c118be951bb91f5342b1f25451779f806a
tor bjurbom <bjurbom1@gmail.com>
2018-08-17 20:59:02 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+
+
+const port = process.env.PORT || 3000
+var app = express();
+
+
+const publicPath = path.join(__dirname,'../public');
+
+console.log(__dirname + '/../public');
+console.log(publicPath);
+
+
+
+app.use(express.static(publicPath));
+
+//index filen
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`server is running on ${port}`)
+});
