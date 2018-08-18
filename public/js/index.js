4db7607752dbfdae054c6dded6d4c1a63f63e461
tor bjurbom <bjurbom1@gmail.com>
2018-08-18 23:43:18 +0200

    added geolocation to the chat app

--- public/js/index.js
+++ public/js/index.js
@@ -25,6 +25,18 @@ socket.on('disconnect', function() {
 });
 
 
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
+
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
@@ -39,4 +51,20 @@ jQuery('#message-form').on('submit', function (e) {
     })
 });
 
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(){
+
+    if(navigator.geolocation == null){
+        return alert("Geolocation not supported by your browser");
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        socket.emit('createLocationMessage' , {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, function (e) {
+        alert('unable to fetch location');
+    })
+})
 
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message')
+const {generateMessage, generateLocationMessage} = require('./utils/message')
 
 const port = process.env.PORT || 3000
 var app = express();
@@ -43,10 +43,16 @@ io.on('connection' , (socket) =>{
 
     });
 
+
+    socket.on('createLocationMessage', (coords) =>{
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    });
+
     
 
     socket.on('disconnect' , () =>{
         console.log("User have disconnected");
+        socket.broadcast.emit('newMessage' , generateMessage("Admin" ,"User has left"));
     });
  });
 
