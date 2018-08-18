802975d35f256206f8fed054d3280a75e62d2a6e
tor bjurbom <bjurbom1@gmail.com>
2018-08-18 13:46:04 +0200

    added utilites

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message')
 
 const port = process.env.PORT || 3000
 var app = express();
@@ -23,27 +24,15 @@ app.use(express.static(publicPath));
 io.on('connection' , (socket) =>{
  console.log('New user connected');
 
-    socket.emit('newMessage',{
-        from: "admin",
-        text: "welcome to the chat app",
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage',generateMessage("Admin" , "Welcome to the chat app"));
 
-    socket.broadcast.emit('newMessage',{
-        from: "admin",
-        text: "New user joined",
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage("Admin" , "New user joined"));
     
 
 
     socket.on('createEmail', (newEmail) =>{
         console.log(newEmail);
-        io.emit('newMessage',{
-            from: newEmail.from,
-            text: newEmail.text,
-            createdAt : new Date().getTime()
-        })
+        io.emit('newMessage',generateMessage(newEmail.from,newEmail.text))
         /*socket.broadcast.emit('newMessage', {
             from: newEmail.from,
             text: newEmail.text,
